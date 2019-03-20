0de3a0ccfcae089f87bf83df82d9241e8e82e310
Charalampos Gkikas <gkikas@idika.gr>
2019-03-20 19:40:39 +0200

    html output, and zodiac nonsense

--- pregnancy.py
+++ pregnancy.py
@@ -1,15 +1,20 @@
 import argparse
 import datetime
+import html
 import json
 import locale
 import math
+import random
 import shutil
 import smtplib
 import sys
 import tempfile
+import textwrap
 import time
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+import pyaztro
 import zodiac_sign
 from dateutil.parser import parse
 from dateutil.relativedelta import relativedelta
@@ -52,9 +57,7 @@ def main(input_data, out_file):
     conception_min = parse(input_data["min_possible_conception"]).date()
     conception_max = parse(input_data["max_possible_conception"]).date()
 
-    # 3/4/2019 Επίσκεψη γιατρού me πρώτη εκτίμηση και εκτύπωση σε χαρτί
-    # Εκτίμηση γιατρού
-
+    # Εκτιμήσεις γιατρού
     estimations = [(parse(est["date"]).date(), relativedelta(
         weeks=est["weeks"], days=est["days"]))
         for est in input_data["doctors_age_estimations"]]
@@ -63,10 +66,11 @@ def main(input_data, out_file):
         a_date, age) for a_date, age in estimations]
 
     if possible_start_gestation:
-        print("Doctor’s estimates for the start of the pregnacy (gestation):", file=out_file)
+        print(
+            "Doctor’s estimates for the start of the pregnacy (gestation):", file=out_file)
         for i, doctors_conception in enumerate(possible_start_gestation):
             print("\t%s estimate:" % (ordinal(i+1)),
-                doctors_conception, file=out_file)
+                  doctors_conception, file=out_file)
         print(screen_line, file=out_file)
         doctors_age = datetime.date.today() - possible_start_gestation[-1]
         fetal_age_doctors = doctors_age + relativedelta(weeks=-2)
@@ -81,7 +85,7 @@ def main(input_data, out_file):
           divmod(gestational_age.days, 7), file=out_file)
     if doctors_age and doctors_age != gestational_age:
         print("\tDoctor’s last estimate of gestational age (Weeks, Days):",
-            divmod(doctors_age.days, 7), file=out_file)
+              divmod(doctors_age.days, 7), file=out_file)
     print("Fetal Age:", file=out_file)
     print("\tMax fetal age (Weeks, Days):", divmod(
         fetal_age_max.days, 7), file=out_file)
@@ -91,34 +95,33 @@ def main(input_data, out_file):
           divmod(fetal_age_aprox.days, 7), file=out_file)
     if fetal_age_doctors and fetal_age_doctors != fetal_age_aprox:
         print("\tApproximation of fetal age based on doctor’s last estimate (Week, Days):",
-            divmod(fetal_age_doctors.days, 7), file=out_file)
+              divmod(fetal_age_doctors.days, 7), file=out_file)
     print(screen_line, file=out_file)
 
     loc = locale.getlocale()
     locale.setlocale(locale.LC_ALL, 'C')
     delivery_dates = ["%s %s [%s]" % (dt.strftime("%A"), dt.isoformat(
     ), zodiac_sign.get_zodiac_sign(dt)) for dt in fuzzy_delivery_date(last_period)]
-    chinese_zodiacs = set([chinese_zodiac.calculate_dt(dt)
-                           for dt in fuzzy_delivery_date(last_period)])
     naegele_date = naegele_due_date(last_period)
+    possible_zodiac = zodiac_sign.get_zodiac_sign(naegele_date)
 
     print("Possible Delivery Dates:", ' - '.join(delivery_dates), file=out_file)
     print("Due Date (Naegele’s rule):", "%s %s [%s]" % (naegele_date.strftime(
-        "%A"), naegele_date.isoformat(), zodiac_sign.get_zodiac_sign(naegele_date)), file=out_file)
+        "%A"), naegele_date.isoformat(), possible_zodiac), file=out_file)
     # My prognosis
     if possible_start_gestation and possible_start_gestation[-1] != last_period:
         harrys_date = naegele_due_date(possible_start_gestation[-1])
+        possible_zodiac = zodiac_sign.get_zodiac_sign(harrys_date)
         print("Harry’s prediction of due date:", "%s %s [%s]" % (harrys_date.strftime(
-            "%A"), harrys_date.isoformat(), zodiac_sign.get_zodiac_sign(harrys_date)), file=out_file)
-    print("Chinese zodiac:", " or ".join(chinese_zodiacs), file=out_file)
+            "%A"), harrys_date.isoformat(), possible_zodiac), file=out_file)
     print(screen_line, file=out_file)
     locale.setlocale(locale.LC_ALL, loc)
-    
+
     try:
         total_days = (harrys_date - last_period).days
     except UnboundLocalError:
         total_days = (naegele_date - last_period).days
-        
+
     completed_days = (datetime.date.today() - last_period).days
     if out_file is sys.stdout:
         # Animation of Progress Bar
@@ -129,15 +132,48 @@ def main(input_data, out_file):
     else:
         print("Pregnacy is about %.1f%% complete" %
               (completed_days*100/total_days), file=out_file)
+    print(screen_line, file=out_file)
+
+    print("", file=out_file)
+    print("Silly & Fun Stuff".center(len(screen_line), '*'), file=out_file)
+    print("", file=out_file)
+    chinese_zodiacs = set([chinese_zodiac.calculate_dt(dt)
+                           for dt in fuzzy_delivery_date(last_period)])
+    chinese_desc = "Chinese zodiac: %s" % (" or ".join(chinese_zodiacs))
+    print(textwrap.fill(chinese_desc, width=len(screen_line)), file=out_file)
+    print("", file=out_file)
+    horoscope = pyaztro.Aztro(sign=possible_zodiac.lower())
+    horoscope_desc = "Horoscope for %s: %s" % (
+        horoscope.sign.capitalize(), horoscope.description)
+    print(textwrap.fill(horoscope_desc, width=len(screen_line)), file=out_file)
+    print("", file=out_file)
+    print("Color:", horoscope.color, file=out_file)
+    print("Mood:", horoscope.mood, file=out_file)
+    print("Compatibility:", horoscope.compatibility, file=out_file)
+    print("Lucky Number:", horoscope.lucky_number, file=out_file)
+    print("Lucky Time:", horoscope.lucky_time, file=out_file)
 
 
 def send_email(email_info, email_msg):
     commaspace = ', '
-    msg = MIMEText(email_msg)
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
     msg['Subject'] = 'Pregnancy Statistics for: %s' % (
         datetime.date.today().strftime("%x"))
     msg['From'] = email_info["email_from"]
     msg['To'] = commaspace.join(email_info["email_rcpt_to"])
+
+    # Record the MIME types of both parts - text/plain and text/html.
+    html_text = '<html><head></head><body><pre width="79">%s</pre></body></html>' % (
+        html.escape(email_msg))
+    part1 = MIMEText(email_msg, 'plain')
+    part2 = MIMEText(html_text, 'html')
+
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    msg.attach(part1)
+    msg.attach(part2)
 
     with smtplib.SMTP(email_info["email_smtp_host"], email_info["email_smtp_port"]) as s:
         if email_info["email_smtp_tls"]:
